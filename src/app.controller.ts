572f9a3519e865f79490583945180d803bdd93e9
JuksAgellon <juliannekyla.agellon@cvsu.edu.ph>
2024-11-10 16:00:26 +0800

    Completed Assignment 1: Fibonacci Sequence

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,29 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get(':n') // Ensure this is correct
+    generateFibonacci(@Param('n') n: string): { sequence: number[] } {
+        const numberN = parseInt(n, 10);
+        if (isNaN(numberN) || numberN < 1) {
+            throw new BadRequestException('Please provide a valid positive integer for n.');
+        }
+        const sequence = this.fibonacci(numberN);
+        return { sequence };
+    }
+
+    private fibonacci(n: number): number[] {
+        const sequence: number[] = [];
+        let a = 0, b = 1;
+
+        for (let i = 0; i < n; i++) {
+            sequence.push(a);
+            const next = a + b;
+            a = b;
+            b = next;
+        }
+
+        return sequence;
+    }
+
 }
